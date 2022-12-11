d7cdcb9c51e06eaa5ab7e0aeecd230a502339279
Lea Briskman <olga1998_01@mail.ru>
2022-12-12 00:29:49 +0200

    desktop version

---
+++ script.js
@@ -0,0 +1,7 @@
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
+
+menuBtn.addEventListener('click', function(){
+	menuBtn.classList.toggle('active');
+	menu.classList.toggle('active');
+});
