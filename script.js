ea3e2e62cf19e387bbafbd60926cc2c534f24a6a
Lea Briskman <olga1998_01@mail.ru>
2022-12-27 00:02:01 +0200

    mobile design first version

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
 const menuBtn = document.querySelector('.menu-btn');
 const menu = document.querySelector('.menu');
+const menuItems = document.querySelectorAll('.menu-item');
 
-menuBtn.addEventListener('click', function(){
-	menuBtn.classList.toggle('active');
+const toggleMenu = () => {
+    menuBtn.classList.toggle('active');
 	menu.classList.toggle('active');
+};
+
+menuBtn.addEventListener('click', () => toggleMenu());
+
+menuItems.forEach(item => {
+    item.addEventListener('click', () => toggleMenu())
 });
+
+const smoothLinks = document.querySelectorAll('a[href^="#"]');
+for (let smoothLink of smoothLinks) {
+    smoothLink.addEventListener('click', function (e) {
+        e.preventDefault();
+        const id = smoothLink.getAttribute('href');
+
+        document.querySelector(id).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        });
+    });
+};
