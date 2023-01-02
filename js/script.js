fb7b9c9318a2e83ecec088c127e624cd6036e847
Lea Briskman <olga1998_01@mail.ru>
2023-01-02 23:02:33 +0200

    styles and modals

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// menu
 const menuBtn = document.querySelector('.menu-btn');
 const menu = document.querySelector('.menu');
 const menuItems = document.querySelectorAll('.menu-item');
@@ -13,6 +14,7 @@ menuItems.forEach(item => {
     item.addEventListener('click', () => toggleMenu())
 });
 
+// smooth scroll
 const smoothLinks = document.querySelectorAll('a[href^="#"]');
 for (let smoothLink of smoothLinks) {
     smoothLink.addEventListener('click', function (e) {
@@ -26,22 +28,68 @@ for (let smoothLink of smoothLinks) {
     });
 };
 
-document.addEventListener('DOMContentLoaded', () => {
-    const splide = new Splide('.splide', {
-        type: 'slide',
-        focus: 'center',
-        perPage: 1,
-        perMove: 1,
-        gap: 30,
-        arrows: true,
-        pagination: false,
-        updateOnMove: true,
-        breakpoints: {
-            1024: {
-                perPage: 1,
-                type: 'slide',
-            },
-        }
-    });
-    splide.mount();
-});
+// modals
+// btns
+const flatsBtn = document.getElementById("flats");
+const housesBtn = document.getElementById("houses");
+const officesBtn = document.getElementById("offices");
+const industrialsBtn = document.getElementById("industrials");
+const requestBtn = document.getElementById("requestBtn");
+const projectBtn = document.getElementById("sendProject");
+
+// modal windows
+const flatsModal = document.getElementById("flatsModal");
+const housesModal = document.getElementById("housesModal");
+const officesModal = document.getElementById("officesModal");
+const industrialsModal = document.getElementById("industrialsModal");
+const requestModal = document.getElementById("requestModal");
+const projectModal = document.getElementById("projectModal");
+
+// open modals
+flatsBtn.onclick = () => flatsModal.style.display = "flex";
+housesBtn.onclick = () => housesModal.style.display = "flex";
+officesBtn.onclick = () => officesModal.style.display = "flex";
+industrialsBtn.onclick = () => industrialsModal.style.display = "flex";
+requestBtn.onclick = () => {
+    requestModal.style.display = "flex";
+    toggleMenu();
+};
+projectBtn.onclick = () => projectModal.style.display = "flex";
+
+// close modals by click on x
+const flatsClose = document.getElementById("flatsClose");
+const housesClose = document.getElementById("housesClose");
+const officesClose = document.getElementById("officesClose");
+const industrialsClose = document.getElementById("industrialsClose");
+const requestClose = document.getElementById("requestClose");
+const projectClose = document.getElementById("projectClose");
+
+flatsClose.onclick = () => flatsModal.style.display = "none";
+housesClose.onclick = () => housesModal.style.display = "none";
+officesClose.onclick = () => officesModal.style.display = "none";
+industrialsClose.onclick = () => industrialsModal.style.display = "none";
+requestClose.onclick = () => requestModal.style.display = "none";
+projectClose.onclick = () => projectModal.style.display = "none";
+
+// close on outside modal click
+window.onclick = e => {
+  if (e.target == flatsModal || 
+    e.target == housesModal || 
+    e.target == officesModal || 
+    e.target == industrialsModal || 
+    e.target == requestModal ||
+    e.target == projectModal) {
+    flatsModal.style.display = "none";
+    housesModal.style.display = "none";
+    officesModal.style.display = "none";
+    industrialsModal.style.display = "none";
+    requestModal.style.display = "none";
+    projectModal.style.display = "none";
+  }
+};
+
+// photo gallery
+lightGallery(document.getElementById('lightgalleryFlats')); 
+lightGallery(document.getElementById('lightgalleryHouses')); 
+lightGallery(document.getElementById('lightgalleryOffices')); 
+lightGallery(document.getElementById('lightgalleryIndustrials')); 
---
+++ project.php
@@ -0,0 +1,3 @@
+<?php
+    mail('olga1998_01@mail.ru', $_POST['number'], $_POST['name'], $_POST['project']);
+?>
